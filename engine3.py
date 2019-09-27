153de3fb8af44f4d8eb50ec8c15e933baef558b8
benmackenzie-exos <ben.mackenzie@exosfinancial.com>
2019-09-27 14:00:18 -0400

    blah

--- engine3.py
+++ engine3.py
@@ -1,29 +1,29 @@
 model_inputs = {
         "growth_rate": 0.06,
         "income_rate": 0.02,
-        "inflation": 0.03,
+        "inflation": 0.0,
         "start_year": 2019,
         "age": 65,
         "spouse_age": 60,
-        "regularAsset": 10000,
+        "regularAsset": 500000,
         "regularAssetSp": 1000000,
         "regularAssetBookValue": 0,
         "regularAssetSpBookValue": 0,
-        "rrsp": 0,
+        "rrsp": 100000,
         "rrspSp": 0,
         "rrif": 100000,
         "rrifSp": 0,
         "end_year": 2030,
-        "end_balance": 200000,
+        "end_balance": 500000,
         "tax_rate": 0.5,
-        "income_requirements": 50000,
+        "income_requirements": 70000,
         "pension_income": 10000
     }
 
 
-transactions = []
 
-def createTransaction(type, account, amount, desc=""):
+
+def createTransaction(transactions, type, account, amount, desc=""):
     transactions.append(
         {
             "type"  : type,
@@ -70,28 +70,28 @@ def process_transactions(book, transactions):
     return book
 
 
-def rrsp_converstion_to_rrif(book, client):
+def rrsp_converstion_to_rrif(transactions, book, client):
     if client:
         rrspAccount = "rrspAccount"
         rrifAccount = "rrifAccount"
 
-    createTransaction("debit", rrspAccount, book[rrspAccount], "rrsp conversion")
-    createTransaction("credit", rrifAccount, book[rrspAccount], "rrsp conversion")
+    createTransaction(transactions, "debit", rrspAccount, book[rrspAccount], "rrsp conversion")
+    createTransaction(transactions, "credit", rrifAccount, book[rrspAccount], "rrsp conversion")
 
 
 
 
-def get_mandatory_rrif_withdrawals(book, year):
+def get_mandatory_rrif_withdrawals(transactions, book, year):
     if (model_inputs["age"] + year > 65) and (book["rrif"] > 0):
-        createTransaction("debit", "rrif", book["rrif"] / 20, "mandatory rrif withdrawal")
-        createTransaction("credit", "clearing", book["rrif"] / 20, "mandatory rrif withdrawal")
+        createTransaction(transactions,"debit", "rrif", book["rrif"] / 20, "mandatory rrif withdrawal")
+        createTransaction(transactions,"credit", "clearing", book["rrif"] / 20, "mandatory rrif withdrawal")
     if (model_inputs["spouse_age"] + year > 65) and (book["rrifSp"] > 0):
-        createTransaction("debit", "rrifSp", book["rrifSp"] / 20, "mandatory rrif withdrawal")
-        createTransaction("credit", "clearing", book["rrifSp"] / 20, "mandatory rrif withdrawal")
+        createTransaction(transactions, "debit", "rrifSp", book["rrifSp"] / 20, "mandatory rrif withdrawal")
+        createTransaction(transactions, "credit", "clearing", book["rrifSp"] / 20, "mandatory rrif withdrawal")
 
 
 
-def sell_regular_asset(client, book, amount):
+def sell_regular_asset(transactions, client, book, amount):
 
     if client:
         account = "regularAsset"
@@ -103,10 +103,10 @@ def sell_regular_asset(client, book, amount):
     total = book[account]
     bookvalue = book[bookValue]
 
-    createTransaction("debit", account, amount, "sell asset")
-    createTransaction("debit", bookValue, (amount / total) * bookvalue)
-    createTransaction("credit", "clearing", amount)
-    createTransaction("debit", "clearing", (amount * (total - bookvalue) / total) * model_inputs["tax_rate"], "tax on sale of asset" )
+    createTransaction(transactions,"debit", account, amount, "sell asset")
+    createTransaction(transactions,"debit", bookValue, (amount / total) * bookvalue)
+    createTransaction(transactions,"credit", "clearing", amount)
+    createTransaction(transactions,"debit", "clearing", (amount * (total - bookvalue) / total) * model_inputs["tax_rate"], "tax on sale of asset" )
 
 
 
@@ -115,27 +115,27 @@ def sell_regular_asset(client, book, amount):
 
 
 
-def generate_base_transactions(current_book):
+def generate_base_transactions(transactions, current_book):
 
     year = current_book["year"]
 
-    createTransaction("debit", "clearing",
+    createTransaction(transactions, "debit", "clearing",
                       get_future_value(year, model_inputs["pension_income"], model_inputs["inflation"]),
                       "pension_income")
 
     for account in ["regularAsset", "regularAssetSp"]:
-        createTransaction("credit", account, current_book[account] * model_inputs["growth_rate"], "capital appreciation")
-        createTransaction("credit", "clearing", current_book[account] * model_inputs["income_rate"], "dividends and interest")
-        createTransaction("debit", "clearing",current_book[account] * model_inputs["income_rate"] * model_inputs["tax_rate"], "tax on dividends and interest" )
+        createTransaction(transactions,"credit", account, current_book[account] * model_inputs["growth_rate"], "capital appreciation")
+        createTransaction(transactions,"credit", "clearing", current_book[account] * model_inputs["income_rate"], "dividends and interest")
+        createTransaction(transactions,"debit", "clearing",current_book[account] * model_inputs["income_rate"] * model_inputs["tax_rate"], "tax on dividends and interest" )
 
     for account in ["rrsp", "rrspSp"]:
         if (current_book[account] > 0):
-            createTransaction("credit", account, current_book[account] * model_inputs["growth_rate"], "growth")
-            createTransaction("credit", account, current_book[account] * model_inputs["income_rate"], "interest and dividend")
+            createTransaction(transactions,"credit", account, current_book[account] * model_inputs["growth_rate"], "growth")
+            createTransaction(transactions,"credit", account, current_book[account] * model_inputs["income_rate"], "interest and dividend")
 
 
-    get_mandatory_rrif_withdrawals(current_book, year)
-    createTransaction("debit", "clearing", get_future_value(year, model_inputs["income_requirements"], model_inputs["inflation"]), "living expense")
+    get_mandatory_rrif_withdrawals(transactions,current_book, year)
+    createTransaction(transactions,"debit", "clearing", get_future_value(year, model_inputs["income_requirements"], model_inputs["inflation"]), "living expense")
 
 
 
@@ -156,7 +156,7 @@ def amount_of_regular_asset_to_sell(book, client, need):
     return x
 
 
-def meet_cash_req_from_regular_asset(book, client):
+def meet_cash_req_from_regular_asset(transactions,book, client):
     if client:
         account = "regularAsset"
 
@@ -169,53 +169,152 @@ def meet_cash_req_from_regular_asset(book, client):
 
     regular_asset_needed = amount_of_regular_asset_to_sell(book, client, needs)
     if regular_asset_needed <= book[account]:
-        sell_regular_asset(client, book, regular_asset_needed)
+        sell_regular_asset(transactions, client, book, regular_asset_needed)
     else:
-        sell_regular_asset(client, book, book[account])
+        sell_regular_asset(transactions, client, book, book[account])
 
 
 
-def invest_funds(book):
-    createTransaction("credit", "regularAsset",  book["clearing"], "invest available funds")
-    createTransaction("debit", "clearing",  book["clearing"])
+def invest_funds(transactions,book):
+    createTransaction(transactions,"credit", "regularAsset",  book["clearing"], "invest available funds")
+    createTransaction(transactions,"debit", "clearing",  book["clearing"])
 
 
-
+def get_capital(book):
+    total = 0
+    total += book["regularAsset"]
+    total += book["regularAssetSp"]
+    total += book["rrsp"]
+    total += book["rrspSp"]
+    total += book["rrif"]
+    total += book["rrifSp"]
+    return total
 
 def simulate_one_year(start_book):
-    generate_base_transactions(start_book)
+    transactions = []
+    generate_base_transactions(transactions, start_book)
     book = process_transactions(start_book, transactions)
 
     if book["clearing"] < 0:
-        meet_cash_req_from_regular_asset(book, True)
+        meet_cash_req_from_regular_asset(transactions, book, True)
         book = process_transactions(start_book, transactions)
 
         if book["clearing"] < 0:
-            meet_cash_req_from_regular_asset(book, False)
+            meet_cash_req_from_regular_asset(transactions, book, False)
             book = process_transactions(start_book, transactions)
     else:
-        invest_funds(book)
+        invest_funds(transactions, book)
 
     book = process_transactions(start_book, transactions)
     book["year"] += 1
     return book
 
 
-def create_projection():
+def create_projection(book):
     sim = []
-    book = get_start_book()
     sim.append(book)
 
     for i in range(model_inputs["start_year"], model_inputs["end_year"]):
         book = simulate_one_year(book)
         sim.append(book)
 
-    for s in sim:
-        print(s)
+    return get_capital(book), sim
 
 
 
-create_projection()
+#todo..adjust bookvalue aswell
+
+
+def set_essential_capital(book, essential_capital):
+    transactions = []
+
+    delta = get_capital(book) - essential_capital
+
+    if delta <= 0:
+        return transactions
+
+    if book["regularAsset"] > 0:
+        if book["regularAsset"] > delta:
+            createTransaction(transactions, "debit", "regularAsset", delta, "remove surplus capital")
+            delta = 0
+        else:
+            createTransaction(transactions, "debit", "regularAsset", book["regularAsset"], "remove surplus capital")
+            delta -= book["regularAsset"]
+
+    if delta == 0:
+        return transactions
+
+    if book["regularAssetSp"] > 0:
+        if book["regularAssetSp"] > delta:
+            createTransaction(transactions, "debit", "regularAssetSp", delta, "remove surplus capital")
+            delta = 0
+        else:
+            createTransaction(transactions, "debit", "regularAssetSp", book["regularAssetSp"], "remove surplus capital")
+            delta -= book["regularAssetSp"]
+
+    if delta == 0:
+        return transactions
+
+    if book["rrsp"] > 0:
+        if book["rrsp"] > delta:
+            createTransaction(transactions, "debit", "rrsp", delta, "remove surplus capital")
+            delta = 0
+        else:
+            createTransaction(transactions, "debit", "rrsp", book["rrsp"], "remove surplus capital")
+            delta -= book["rrsp"]
+
+    if delta == 0:
+        return transactions
+
+    if book["rrspSp"] > 0:
+        if book["rrspSp"] > delta:
+            createTransaction(transactions, "debit", "rrspSp", delta, "remove surplus capital")
+            delta = 0
+        else:
+            createTransaction(transactions, "debit", "rrspSp", book["rrspSp"], "remove surplus capital")
+            delta -= book["rrspSp"]
+
+def print_list(list):
+    for l  in list:
+        print(l)
+
+
+def find_essential_capital():
+
+    book = get_start_book()
+    low = 0
+    high = get_capital(book)
+    essential_capital = high
+    desired_end_balance = get_future_value(model_inputs["end_year"], model_inputs["end_balance"], model_inputs["inflation"])
+
+    end_capital, sim = create_projection(book)
+    if end_capital < desired_end_balance:
+        print("no surplus")
+        return end_capital, sim
+
+
+    while True:
+        book = get_start_book()
+        surplus_cap_transactions = set_essential_capital(book, essential_capital)
+        book = process_transactions(book, surplus_cap_transactions)
+
+        end_capital, sim = create_projection(book)
+
+        if abs(desired_end_balance - end_capital) < 1:
+            return sim, surplus_cap_transactions
+        elif end_capital > desired_end_balance:
+            high = essential_capital
+            essential_capital = (high + low) / 2
+        else:
+            low = essential_capital
+            essential_capital = (high + low) / 2
+
+
+
+sim, sc = find_essential_capital()
+print_list(sim)
+print_list(sc)
+print(get_capital(sim[-1]))
 
 #todo...mandatory rrsp conversion to rrif, sale of house, receipt of asset.
 
