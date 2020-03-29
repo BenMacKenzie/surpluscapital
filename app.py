6f62b53ef0707d1ba31a46bc35ee947541d2b7fc
benmackenzie <ben.mackenzie@thinkbiganalytics.com>
2020-03-29 11:03:44 -0400

    fix dash

--- app.py
+++ app.py
@@ -2,24 +2,31 @@
 import os
 from random import randint
 
-import plotly.plotly as py
-from plotly.graph_objs import *
 
 import flask
 import dash
-from dash.dependencies import Input, Output, State, Event
+import dash_html_components as dhc
 import dash_core_components as dcc
-import dash_html_components as html
+
+
 
 
 # Setup the app
 # Make sure not to change this file name or the variable names below,
 # the template is configured to execute 'server' on 'app.py'
 server = flask.Flask(__name__)
-server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
-app = dash.Dash(__name__, server=server)
+#server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+app = dash.Dash('Dash Hello World')
 
+text_style = dict(color='#444', fontFamily='sans-serif', fontWeight=300)
+plotly_figure = dict(data=[dict(x=[1,2,3], y=[2,4,8])])
 
+app.layout = dhc.Div([
+        dhc.H2('Surplus Capital', style=text_style),
+        dhc.P('Enter a Plotly trace type into the text box, such as histogram, bar, or scatter.', style=text_style),
+        dcc.Input(id='text1', placeholder='box', value=''),
+        dcc.Graph(id='plot1', figure=plotly_figure),
+    ])
 # Put your Dash code here
 
 
