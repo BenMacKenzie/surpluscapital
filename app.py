56f5858413af9d10b5acd57881fc73b64341588e
benmackenzie <ben.mackenzie@thinkbiganalytics.com>
2020-03-31 14:57:14 -0400

    interactive button

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import dash
 import dash_html_components as dhc
 import dash_core_components as dcc
 import plotly.graph_objects as go
+from dash.dependencies import Input, Output, State
+from transasctions import *
 
-import plotly.express as px
-
-from engine import get_essential_capital_projection
+from engine import get_projection
 
 
 app = dash.Dash('Dash Hello World')
@@ -16,21 +16,63 @@ server = app.server
 
 text_style = dict(color='#444', fontFamily='sans-serif', fontWeight=300)
 
-data = get_essential_capital_projection()
+
+
+START_BOOK = {
+        "joint" : {Account.CLEARING: 0,
+                   Account.HOME: 0},
+        "client" : {
+            Account.REGULAR: 0,
+            Account.REGULAR_BOOK_VALUE: 0,
+            Account.TFSA: 0,
+            Account.RRSP: 0,
+            Account.RRIF: 0
+        },
+
+        "spouse": {
+            Account.REGULAR: 0,
+            Account.REGULAR_BOOK_VALUE: 0,
+            Account.TFSA: 0,
+            Account.RRSP: 0,
+            Account.RRIF: 0
+        },
+
+
+}
 
 
 
-fig = go.Figure(data=[
-    go.Bar(name='essential', x=data["year"], y=data["essential"]),
-    go.Bar(name='surplus', x=data["year"], y=data["surplus"])
-])
+
+
+
 
 app.layout = dhc.Div([
         dhc.H2('Surplus Capital', style=text_style),
-        dhc.P('Enter desired end capital', style=text_style),
-        dcc.Input(id='text1', placeholder='box', value=''),
-        dcc.Graph(id='plot1', figure=fig),
+        dhc.P('Regular Asset Account Balance', style=text_style),
+        dcc.Input(id='Regular_Account_Balance', placeholder='0', value='100000'),
+        dhc.Button('Calculate', id='calculate_button'),
+        dcc.Graph(id='plot1'),
     ])
+
+
+
+@app.callback(
+    Output("plot1", "figure"), [Input("calculate_button", "n_clicks")], state=[State("Regular_Account_Balance", "value")])
+def update_graph(n, end_balance):
+    start_book = START_BOOK
+    start_book["client"][Account.REGULAR] = int(end_balance)
+    _, _, _, data = get_projection(start_book=start_book)
+
+    fig = go.Figure(data=[
+        go.Bar(name='essential', x=data["year"], y=data["essential"]),
+        go.Bar(name='surplus', x=data["year"], y=data["surplus"])
+    ])
+
+    return fig
+
+
+
+
 # Put your Dash code here
 
 
--- engine.py
+++ engine.py
@@ -5,222 +5,15 @@ import pandas as pd
 
 
 
-def simulate_one_year(start_book, year):
-    transactions = []
-    generate_base_transactions(transactions, start_book, parameters)
-    book = process_transactions(start_book, transactions)
-
-    if book['joint'][Account.CLEARING] > 0:
-        invest_funds(transactions, book)
-
-    else:
-        #client regular asset
-        meet_cash_req_from_regular_asset(transactions, book, "client", parameters["tax_rate"])
-        book = process_transactions(start_book, transactions)
-
-        #spouse regular asset
-        if book['joint'][Account.CLEARING] < 0:
-            meet_cash_req_from_regular_asset(transactions, book, "spouse", parameters["tax_rate"])
-            book = process_transactions(start_book, transactions)
-
-        # client tfsa
-        if book['joint'][Account.CLEARING] < 0:
-            meet_cash_req_from_tfsa(transactions, book, "client")
-            book = process_transactions(start_book, transactions)
-
-        #spouse tfsa
-        if book['joint'][Account.CLEARING] < 0:
-            meet_cash_req_from_tfsa(transactions, book, "spouse")
-            book = process_transactions(start_book, transactions)
-
-        #client rrsp
-        if book['joint'][Account.CLEARING] < 0:
-            meet_cash_req_from_deferred(transactions, book, "client", Account.RRSP, parameters["tax_rate"])
-            book = process_transactions(start_book, transactions)
-
-        #spouse rrsp
-        if book['joint'][Account.CLEARING] < 0:
-            meet_cash_req_from_deferred(transactions, book, "spouse", Account.RRSP, parameters["tax_rate"])
-            book = process_transactions(start_book, transactions)
-
-        # client rrif
-        if book['joint'][Account.CLEARING] < 0:
-            meet_cash_req_from_deferred(transactions, book, "client", Account.RRIF, ["tax_rate"])
-            book = process_transactions(start_book, transactions)
-
-        # spouse rrif
-        if book['joint'][Account.CLEARING] < 0:
-            meet_cash_req_from_deferred(transactions, book, "spouse", Account.RRIF, parameters["tax_rate"])
-            _ = process_transactions(start_book, transactions)
-
-
-
-    book = process_transactions(start_book, transactions)
-    book["year"] = year
-    book["transactions"] = transactions
-    return book
-
-
-
-def create_projection(book, sc=None):
-    sim = []
-
-    if sc:
-        book["transactions"] = sc
-
-    for i in range(parameters["start_year"], parameters["end_year"]+1):
-        d = {}
-        d['start'] = book
-        book = simulate_one_year(book, i)
-        d['end'] = book
-        sim.append(d)
-
-        #this become starting book for next year..just as reference
-        book = book.copy()
-        book['year'] += 1
-        book.pop('transactions')
-
-
-
-    return get_capital(d['end']), sim
-
-
-
-
-
-#set level of essential capital by debiting accounts...note that the meaning of the essential capital is dependend on order
-
-def set_essential_capital(book, essential_capital):
-    transactions = []
-
-    delta = get_capital(book) - essential_capital
-
-    if delta <= 0:
-        return transactions
-
-    if parameters['spouse']:
-        persons = ['client', 'spouse']
-    else:
-        persons = ['client']
-
-
-    for person in persons:
-        if book[person][Account.REGULAR] > delta:
-            createTransaction(transactions, "debit", person, Account.REGULAR, delta, Transaction.REMOVE_SURPLUS_CAPITAL)
-            createTransaction(transactions, "debit", person, Account.REGULAR_BOOK_VALUE, delta * book[person][Account.REGULAR_BOOK_VALUE]/ book[person][Account.REGULAR],
-                              Transaction.REMOVE_SURPLUS_CAPITAL)
-            delta = 0
-
-        elif book[person][Account.REGULAR] > 0:
-            createTransaction(transactions, "debit", person, Account.REGULAR, book[person][Account.REGULAR], Transaction.REMOVE_SURPLUS_CAPITAL)
-            createTransaction(transactions, "debit", person, Account.REGULAR_BOOK_VALUE, book[person][Account.REGULAR_BOOK_VALUE],Transaction.REMOVE_SURPLUS_CAPITAL)
-            delta -= book[person][Account.REGULAR]
-
-        if delta == 0:
-            return transactions
-
-
-    for person in persons:
-
-        if book[person][Account.TFSA] > delta:
-            createTransaction(transactions, "debit", person, Account.TFSA, delta, Transaction.REMOVE_SURPLUS_CAPITAL)
-            delta = 0
-
-        elif book[person][Account.TFSA] > 0:
-            createTransaction(transactions, "debit", person, Account.TFSA, book[person][Account.TFSA],
-                              Transaction.REMOVE_SURPLUS_CAPITAL)
-
-            delta -= book[person][Account.TFSA]
-
-        if delta == 0:
-            return transactions
-
-
-    for person in persons:
-
-        if book[person][Account.RRSP] > delta:
-            createTransaction(transactions, "debit", person, Account.RRSP, delta, Transaction.REMOVE_SURPLUS_CAPITAL)
-            delta = 0
-
-        elif book[person][Account.RRSP] > 0:
-            createTransaction(transactions, "debit", person, Account.RRSP, book[person][Account.RRSP],
-                              Transaction.REMOVE_SURPLUS_CAPITAL)
-
-            delta -= book[person][Account.RRSP]
-
-        if delta == 0:
-            return transactions
-
-
-    for person in persons:
-
-        if book[person][Account.RRIF] > delta:
-            createTransaction(transactions, "debit", person, Account.RRIF, delta, Transaction.REMOVE_SURPLUS_CAPITAL)
-            delta = 0
-
-        elif book[person][Account.RRIF] > 0:
-            createTransaction(transactions, "debit", person, Account.RRIF, book[person][Account.RRIF],
-                              Transaction.REMOVE_SURPLUS_CAPITAL)
-
-            delta -= book[person][Account.RRIF]
-
-        if delta == 0:
-            return transactions
-
-
-    return transactions
-
-
-
-
-
-def find_essential_capital(start_book):
-    low = 0
-    high = get_capital(start_book)
-    essential_capital = high
-    desired_end_balance = get_future_value(parameters["start_year"],parameters["end_year"], parameters["end_balance"], parameters["inflation"])
-
-    end_capital, sim = create_projection(start_book)
-    if end_capital < desired_end_balance:
-        print("no surplus")
-        return sim, []
-
-
-
-    while True:
-        book = start_book
-        surplus_cap_transactions = set_essential_capital(book, essential_capital)
-        book = process_transactions(book, surplus_cap_transactions)
-
-        end_capital, sim = create_projection(book, surplus_cap_transactions)
-
-        print(end_capital)
-
-        #close enough
-        if abs(desired_end_balance - end_capital) < 1000:
-            return sim, surplus_cap_transactions
-
-        #there is no essential capital..all surplus
-        elif (end_capital > desired_end_balance) and (essential_capital < 1):
-            return sim, surplus_cap_transactions
-
-        elif end_capital > desired_end_balance:
-            high = essential_capital
-            essential_capital = round((high + low) / 2,0)
-        else:
-            low = essential_capital
-            essential_capital = round((high + low) / 2,0)
-
-
-parameters = {
+PARAMETERS = {
         "growth_rate": 0.05,
         "income_rate": 0.02,
         "inflation": 0.02,
-        "start_year": 2019,
+        "start_year": 2020,
         "client_age": 67,
         "spouse": True,
         "spouse_age": 63,
-        "end_year": 2043,
+        "end_year": 2044,
         "end_balance": 100000,
         "tax_rate": 0.40,
         "pensions": [
@@ -264,7 +57,7 @@ parameters = {
         "income_requirements": 140000,
         }
 
-start_book = {
+START_BOOK = {
         "joint" : {Account.CLEARING: 0,
                    Account.HOME: 0},
         "client" : {
@@ -284,10 +77,9 @@ start_book = {
         },
 
 
-        "year": parameters["start_year"],
 }
 
-start_surplus_capital_book = {
+START_SURPLUS_CAPITAL_BOOK = {
     "joint": {Account.CLEARING: 0,
               Account.HOME: 0},
     "client": {
@@ -306,12 +98,212 @@ start_surplus_capital_book = {
         Account.RRIF: 0
     },
 
-    "year": parameters["start_year"],
 }
 
+def get_projection(parameters=PARAMETERS, start_book=START_BOOK):
+    start_surplus_capital_book= START_SURPLUS_CAPITAL_BOOK
+    start_surplus_capital_book['year'] = parameters["start_year"]
+    start_book["year"]=parameters["start_year"]
 
 
-def get_projection():
+    def simulate_one_year(start_book, year):
+        transactions = []
+        generate_base_transactions(transactions, start_book, parameters)
+        book = process_transactions(start_book, transactions)
+
+        if book['joint'][Account.CLEARING] > 0:
+            invest_funds(transactions, book)
+
+        else:
+            #client regular asset
+            meet_cash_req_from_regular_asset(transactions, book, "client", parameters["tax_rate"])
+            book = process_transactions(start_book, transactions)
+
+            #spouse regular asset
+            if book['joint'][Account.CLEARING] < 0:
+                meet_cash_req_from_regular_asset(transactions, book, "spouse", parameters["tax_rate"])
+                book = process_transactions(start_book, transactions)
+
+            # client tfsa
+            if book['joint'][Account.CLEARING] < 0:
+                meet_cash_req_from_tfsa(transactions, book, "client")
+                book = process_transactions(start_book, transactions)
+
+            #spouse tfsa
+            if book['joint'][Account.CLEARING] < 0:
+                meet_cash_req_from_tfsa(transactions, book, "spouse")
+                book = process_transactions(start_book, transactions)
+
+            #client rrsp
+            if book['joint'][Account.CLEARING] < 0:
+                meet_cash_req_from_deferred(transactions, book, "client", Account.RRSP, parameters["tax_rate"])
+                book = process_transactions(start_book, transactions)
+
+            #spouse rrsp
+            if book['joint'][Account.CLEARING] < 0:
+                meet_cash_req_from_deferred(transactions, book, "spouse", Account.RRSP, parameters["tax_rate"])
+                book = process_transactions(start_book, transactions)
+
+            # client rrif
+            if book['joint'][Account.CLEARING] < 0:
+                meet_cash_req_from_deferred(transactions, book, "client", Account.RRIF, ["tax_rate"])
+                book = process_transactions(start_book, transactions)
+
+            # spouse rrif
+            if book['joint'][Account.CLEARING] < 0:
+                meet_cash_req_from_deferred(transactions, book, "spouse", Account.RRIF, parameters["tax_rate"])
+                _ = process_transactions(start_book, transactions)
+
+
+
+        book = process_transactions(start_book, transactions)
+        book["year"] = year
+        book["transactions"] = transactions
+        return book
+
+
+
+    def create_projection(book, sc=None):
+        sim = []
+
+        if sc:
+            book["transactions"] = sc
+
+        for i in range(parameters["start_year"], parameters["end_year"]+1):
+            d = {}
+            d['start'] = book
+            book = simulate_one_year(book, i)
+            d['end'] = book
+            sim.append(d)
+
+            #this become starting book for next year..just as reference
+            book = book.copy()
+            book['year'] += 1
+            book.pop('transactions')
+
+
+
+        return get_capital(d['end']), sim
+
+    #set level of essential capital by debiting accounts...note that the meaning of the essential capital is dependend on order
+
+    def set_essential_capital(book, essential_capital):
+        transactions = []
+
+        delta = get_capital(book) - essential_capital
+
+        if delta <= 0:
+            return transactions
+
+        if parameters['spouse']:
+            persons = ['client', 'spouse']
+        else:
+            persons = ['client']
+
+
+        for person in persons:
+            if book[person][Account.REGULAR] > delta:
+                createTransaction(transactions, "debit", person, Account.REGULAR, delta, Transaction.REMOVE_SURPLUS_CAPITAL)
+                createTransaction(transactions, "debit", person, Account.REGULAR_BOOK_VALUE, delta * book[person][Account.REGULAR_BOOK_VALUE]/ book[person][Account.REGULAR],
+                                  Transaction.REMOVE_SURPLUS_CAPITAL)
+                delta = 0
+
+            elif book[person][Account.REGULAR] > 0:
+                createTransaction(transactions, "debit", person, Account.REGULAR, book[person][Account.REGULAR], Transaction.REMOVE_SURPLUS_CAPITAL)
+                createTransaction(transactions, "debit", person, Account.REGULAR_BOOK_VALUE, book[person][Account.REGULAR_BOOK_VALUE],Transaction.REMOVE_SURPLUS_CAPITAL)
+                delta -= book[person][Account.REGULAR]
+
+            if delta == 0:
+                return transactions
+
+
+        for person in persons:
+
+            if book[person][Account.TFSA] > delta:
+                createTransaction(transactions, "debit", person, Account.TFSA, delta, Transaction.REMOVE_SURPLUS_CAPITAL)
+                delta = 0
+
+            elif book[person][Account.TFSA] > 0:
+                createTransaction(transactions, "debit", person, Account.TFSA, book[person][Account.TFSA],
+                                  Transaction.REMOVE_SURPLUS_CAPITAL)
+
+                delta -= book[person][Account.TFSA]
+
+            if delta == 0:
+                return transactions
+
+
+        for person in persons:
+
+            if book[person][Account.RRSP] > delta:
+                createTransaction(transactions, "debit", person, Account.RRSP, delta, Transaction.REMOVE_SURPLUS_CAPITAL)
+                delta = 0
+
+            elif book[person][Account.RRSP] > 0:
+                createTransaction(transactions, "debit", person, Account.RRSP, book[person][Account.RRSP],
+                                  Transaction.REMOVE_SURPLUS_CAPITAL)
+
+                delta -= book[person][Account.RRSP]
+
+            if delta == 0:
+                return transactions
+
+
+        for person in persons:
+
+            if book[person][Account.RRIF] > delta:
+                createTransaction(transactions, "debit", person, Account.RRIF, delta, Transaction.REMOVE_SURPLUS_CAPITAL)
+                delta = 0
+
+            elif book[person][Account.RRIF] > 0:
+                createTransaction(transactions, "debit", person, Account.RRIF, book[person][Account.RRIF],
+                                  Transaction.REMOVE_SURPLUS_CAPITAL)
+
+                delta -= book[person][Account.RRIF]
+
+            if delta == 0:
+                return transactions
+
+
+        return transactions
+
+    def find_essential_capital(start_book):
+        low = 0
+        high = get_capital(start_book)
+        essential_capital = high
+        desired_end_balance = get_future_value(parameters["start_year"],parameters["end_year"], parameters["end_balance"], parameters["inflation"])
+
+        end_capital, sim = create_projection(start_book)
+        if end_capital < desired_end_balance:
+            print("no surplus")
+            return sim, []
+
+
+
+        while True:
+            book = start_book
+            surplus_cap_transactions = set_essential_capital(book, essential_capital)
+            book = process_transactions(book, surplus_cap_transactions)
+
+            end_capital, sim = create_projection(book, surplus_cap_transactions)
+
+            print(end_capital)
+
+            #close enough
+            if abs(desired_end_balance - end_capital) < 1000:
+                return sim, surplus_cap_transactions
+
+            #there is no essential capital..all surplus
+            elif (end_capital > desired_end_balance) and (essential_capital < 1):
+                return sim, surplus_cap_transactions
+
+            elif end_capital > desired_end_balance:
+                high = essential_capital
+                essential_capital = round((high + low) / 2,0)
+            else:
+                low = essential_capital
+                essential_capital = round((high + low) / 2,0)
+
 
     essential_capital_projection, sc_transactions = find_essential_capital(start_book)
 
@@ -328,24 +320,20 @@ def get_projection():
     surplus_book = process_transactions(start_surplus_capital_book, sc_transactions)
     _, surplus_capital_projection = create_projection(surplus_book)
 
-    return sc_transactions, essential_capital_projection, surplus_capital_projection
-
-
-
-
-def get_essential_capital_projection():
-    sc, sim1, sim2 = get_projection()
     a = []
 
-    for i in range(len(sim1)):
-        a.append([sim1[i]["start"]["year"], get_capital(sim1[i]["start"]), get_capital(sim2[i]["start"])])
+    for i in range(len(essential_capital_projection)):
+        a.append([essential_capital_projection[i]["start"]["year"], get_capital(essential_capital_projection[i]["start"]), get_capital(surplus_capital_projection[i]["start"])])
 
-    return pd.DataFrame(a, columns=["year", "essential", "surplus"])
+
+    return sc_transactions, essential_capital_projection, surplus_capital_projection, pd.DataFrame(a, columns=["year", "essential", "surplus"])
+
+
 
 
 
 if __name__ == "__main__":
-    sc, sim1, sim2 = get_projection()
+    sc, sim1, sim2, _ = get_projection()
     print("surplus capital = " + str(get_capital(sim2[0]['end'])))
     print(json.dumps(sc))
     print(json.dumps(sim1))
